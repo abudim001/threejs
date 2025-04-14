c407c7a8461b50d268fda70bdb68530f823072f6
Adem Hussen <abudim001@gmail.com>
2025-04-13 22:24:43 -0700

    Implement ColorPicker tab

--- client/src/components/Tap.tsx
+++ client/src/components/Tap.tsx
@@ -4,20 +4,20 @@ import state from "../store";
 
 const Tap = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
   const snap = useSnapshot(state);
-  const activeStyles =
-    isFilterTab && isActiveTab
-      ? { backgroundColor: snap.color, opacity: 0.5 }
-      : { backgroundColor: "transparent", opacity: 1 };
+
   return (
     <div
       key={tab.name}
-      className={`tab-btn ${
-        isFilterTab ? "rounded-full glassmorphism" : "rounded-4"
-      }`}
+      className={`tab-btn ${isFilterTab ? "" : ""}`}
       onClick={handleClick}
-      style={activeStyles}
     >
-      <img src={tab.icon}></img>
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${
+          isFilterTab ? "w-3/3 h-3/3" : "w-11/12 h-11/12 object-contain"
+        }`}
+      ></img>
     </div>
   );
 };
--- client/src/pages/Customizer.tsx
+++ client/src/pages/Customizer.tsx
@@ -4,14 +4,46 @@ import { snapshot, useSnapshot } from "valtio";
 
 import config from "../config/config";
 import state from "../store";
-import { download } from "../assets";
+import { download, logoShirt, stylishShirt } from "../assets";
 import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, DecalTypes, FilterTabs } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
-import { CustomButton, Tap } from "../components";
+import {
+  AiPicker,
+  ColorPicker,
+  CustomButton,
+  FilePicker,
+  Tap,
+} from "../components";
+import { Color } from "three";
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [gernaraingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // show tab content depending on the active tab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AiPicker />;
+
+      default:
+        return null;
+    }
+  };
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -28,11 +60,15 @@ const Customizer = () => {
                   <Tap
                     key={tap.name}
                     tab={tap}
-                    handleClick={() => {}}
+                    handleClick={() => {
+                      setActiveEditorTab(tap.name);
+                    }}
                     isFilterTab
                     isActiveTab={tap.name}
                   />
                 ))}
+
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
